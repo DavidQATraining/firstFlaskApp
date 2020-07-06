9551d16da5ca9966779517a8cfd821c753678c7d
DavidQATraining <david.j.mccartney@gmail.com>
2020-07-06 16:46:50 +0100

    added layout and made use of it on home page

--- app.py
+++ app.py
@@ -1,11 +1,31 @@
 from flask import Flask
-
+from flask import render_template
 app = Flask(__name__)
 
 
+# if you hit root or home you will still see this functionality
 @app.route('/')
+@app.route('/home')
 def hello_world():
-    return 'Hello Everyone!'
+    return render_template('homepage.html', title='Homepage')
+
+
+@app.route('/about')
+def about():
+    return '''
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title>about</title>
+    </head>
+    <body>
+        <div>
+        <h1>This is about</h1>
+        </div>
+    </body>
+    </html>
+    '''
 
 
 if __name__ == '__main__':
