4fa9f436d8f51599768bc51ee15d3c7ad555b805
DavidQATraining <david.j.mccartney@gmail.com>
2020-07-07 10:37:38 +0100

    added some dummy data and got it to be dynamicaly displayed on the homepage

--- app.py
+++ app.py
@@ -2,30 +2,31 @@ from flask import Flask
 from flask import render_template
 app = Flask(__name__)
 
+dummyData = [
+    {
+        "fName": "David",
+        "lName": "McCartney",
+        "title": "Mr",
+        "content": "some bhoy"
+    },
+    {
+        "fName": "Tadas",
+        "lName": "Vaidotas",
+        "title": "Mr",
+        "content": "lecturer boy"
+    }
+]
 
 # if you hit root or home you will still see this functionality
 @app.route('/')
 @app.route('/home')
 def hello_world():
-    return render_template('homepage.html', title='Homepage')
+    return render_template('homepage.html', title='Homepage', posts=dummyData)
 
 
 @app.route('/about')
 def about():
-    return '''
-    <!DOCTYPE html>
-    <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <title>about</title>
-    </head>
-    <body>
-        <div>
-        <h1>This is about</h1>
-        </div>
-    </body>
-    </html>
-    '''
+    return render_template('aboutpage.html', title='Aboutpage')
 
 
 if __name__ == '__main__':
